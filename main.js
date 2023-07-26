bfbccd3765340f6ce8f53f83d5dd2b7239574765
nocontent06 <spasst.spiel@gmail.com>
2023-07-26 14:46:47 +0200

    Imagine to be too stupid to type "Liquid"

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ let footerElements = document.getElementsByClassName("footer");
 console.log(footerElements);
 
 let version = "1.1";
-let build = "1B7";
+let build = "1B8";
 
 let returnString = `Version ${version} (Build ${build}) \n ©2023 MangoCoding-Inc. All rights reserved.`;
