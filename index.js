a3c9a99af873903a5f82ad6f77c329dca514b0b5
nocontent06 <spasst.spiel@gmail.com>
2023-07-27 02:56:35 +0200

    Added iPhones and iOS Versions!!

--- index.js
+++ index.js
@@ -37,5 +37,17 @@ searchInput.setAttribute("required", true)
 let footer_index = document.createElement("footer");
 footer_index.setAttribute("class", "footer");
 footer_index.innerText = returnString;
+footer_index.innerHTML += '<br> \
+    <a href="https://github.com/nocontent06" target="_blank" style="color: gray">\
+    <i class="fa-brands fa-github" style="font-size: 3vh"></i>\
+    </a>\
+    \
+    <a href="https://discord.gg/hyTP8ynDAz" target="_blank" style="color: gray">\
+    <i class="fa-brands fa-discord" style="font-size: 3vh"></i>\
+    </a><br>\
+    <p style="color: gray">Data provided by \
+    <li>Me, myself and I with all my Macs</li>\
+    <li><a href="https://apple.com" target="_blank" style="color: gray">Apple Inc.</a></li>\
+    <li><a href="https://everymac.com" target="_blank" style="color: gray">EveryMac.com</a></li>'
 
 searchForm.appendChild(footer_index);
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let footerElements = document.getElementsByClassName("footer");
 
 console.log(footerElements);
 
-let version = "1.1";
-let build = "1B7A2";
+let version = "1.1.1";
+let build = "1B8";
 
 let returnString = `Version ${version} (Build ${build}) \n ©2023 MangoCoding-Inc. All rights reserved.`;
