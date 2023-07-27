d73c6f2558e77d8071b6e8785ae0547e64630990
nocontent06 <spasst.spiel@gmail.com>
2023-07-27 18:02:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let footerElements = document.getElementsByClassName("footer");
 
 console.log(footerElements);
 
-let version = "1.1.1";
-let build = "1B8";
+let version = "1.1.2";
+let build = "1B8A1";
 
 let returnString = `Version ${version} (Build ${build}) \n ©2023 MangoCoding-Inc. All rights reserved.`;
