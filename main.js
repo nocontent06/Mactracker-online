847834d0aec7e8894c655944319896e846bcaeab
nocontent06 <spasst.spiel@gmail.com>
2023-08-10 19:49:55 +0200

    Try something :(

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let footerElements = document.getElementsByClassName("footer");
 
 console.log(footerElements);
 
-let version = "1.2 Beta 1";
-let build = "1C9A1";
+
+let version = "1.1.4";
+let build = "1B8A3";
 
 let returnString = `Version ${version} (Build ${build}) \n ©2023 MangoCoding-Inc. All rights reserved.`;
