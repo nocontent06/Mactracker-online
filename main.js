db6159ad02f7aa7eff21a9f0743c366a9e1f6f39
nocontent06 <spasst.spiel@gmail.com>
2023-08-12 23:45:08 +0200

    Went back to stable
    NEW BETA PAGE:
    test.mactracker.online

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ let footerElements = document.getElementsByClassName("footer");
 
 console.log(footerElements);
 
-
-let version = "1.1.4";
-let build = "1B8A3";
+let version = "1.1.5";
+let build = "1B8A4";
 
 let returnString = `Version ${version} (Build ${build}) \n ©2023 MangoCoding-Inc. All rights reserved.`;
