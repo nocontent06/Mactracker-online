410b0fff3644e916ee1ebddd9888e077d43ca86d
Felix <spasst.spiel@gmail.com>
2024-02-27 14:17:02 +0100

    Small bugfix

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let footerElements = document.getElementsByClassName("footer");
 console.log(footerElements);
 
 
-let version = "1.4";
-let build = "1E1c";
+let version = "1.4 (Build 2)";
+let build = "1E1c (2)";
 
 var returnString = `Version ${version} (Build ${build}) \n ©2023 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
