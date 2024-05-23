862cdee10727ef8d37d9ecb6a6a18b0409e9efb9
Felix <spasst.spiel@gmail.com>
2024-05-23 15:52:14 +0200

    Updated Version

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ console.log(footerElements);
 
 
 let version = "1.4.1";
-let build = "1E2";
+let build = "1E2a";
 
 var returnString = `Version ${version} (Build ${build}) \n ©2023 - 2024 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
