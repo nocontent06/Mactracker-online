57d148b6d6581abd80eece4fe903356bdee0cb4b
nocontent06 <spasst.spiel@gmail.com>
2024-12-30 01:34:14 +0100

    Updated beta.mactracker.online info

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ console.log(footerElements);
 
 
 let version = "1.4.1";
-let build = "1E2a";
+let build = "1E2b";
 
 var returnString = `Version ${version} (Build ${build}) \n ©2023 - 2024 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
