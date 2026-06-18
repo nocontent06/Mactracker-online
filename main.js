35172e5e803263ea7c9d0846eb47566bf2c44255
Felix <spasst.spiel@gmail.com>
2026-06-18 12:56:54 +0200

    Updated Version Informations

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let version = "1.5";
-let build = "1F1a";
-let commit = "1 (2026-02-03)";
+let version = "1.5.1";
+let build = "1F2a";
+let commit = "1 (2026-06-18)";
 
 let returnString = `Version ${version} (Build ${build}) \n Commit: ${commit} \n ©2023-2026 MangoCoding-Inc. (Felix) \nAll rights reserved.`;
